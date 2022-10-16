e3197cb952a54b104ca48497d216dd32dd9ef253
MJacob <matheusbendel@hotmail.com>
2022-10-16 20:11:22 -0300

    feature(env): added env variables

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { environment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-      .setTitle('Best Seat')
+      .setTitle(environment.SWAGGER_TITLE)
       .setDescription('API do Best Seat')
       .setVersion('1.0')
       .build();
