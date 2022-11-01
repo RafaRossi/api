f8f970321d511f26f746be175cbfd9b186eb4e3b
MJacob <matheusbendel@hotmail.com>
2022-10-31 22:12:38 -0300

    refactor(main): enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { environment } from './environment/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+  });
+
   const config = new DocumentBuilder()
-      .setTitle(environment.SWAGGER_TITLE)
-      .setDescription('API do Best Seat')
-      .setVersion('1.0')
-      .build();
-  
+    .setTitle(environment.SWAGGER_TITLE)
+    .setDescription('API do Best Seat')
+    .setVersion('1.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
-  
+
   await app.listen(environment.PORT || 3000);
 }
 bootstrap();
