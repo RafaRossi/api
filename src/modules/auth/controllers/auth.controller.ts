b7e3b57f91e636d4823692dee537958d4e1bb98d
MJacob <matheusbendel@hotmail.com>
2022-11-24 20:22:01 -0300

    fix(auth)

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import {Body, Controller, Post} from '@nestjs/common';
 import {ApiOkResponse, ApiOperation, ApiProperty, ApiTags} from "@nestjs/swagger";
+import { UserEntity } from "../../user/entities/user.entity";
 import { AuthPayload } from '../models/auth.payload';
 import { AuthService } from '../services/auth.service';
 
@@ -14,7 +15,7 @@ export class AuthController {
     @Post('/local')
     @ApiOkResponse({ description: 'Usuário logado com sucesso.'})
     @ApiOperation({ summary: 'Autenticação do usuário'})
-    public async auth(@Body() authPayload: AuthPayload): Promise<void> {
+    public async auth(@Body() authPayload: AuthPayload): Promise<UserEntity> {
         return await this.service.auth(authPayload);
     }
     
